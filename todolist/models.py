91f64d0845d65ebf298b7664c03b9776a2585622
danielcm585 <danielcm585@gmail.com>
2022-09-25 16:45:24 +0700

    Finish all features

--- todolist/models.py
+++ todolist/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.forms import ModelForm
 from django.contrib.auth.models import User
 
 class Task(models.Model):
-  done = models.BooleanField(default=False)
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  date = models.DateField(auto_now=True)
-  title = models.CharField(max_length=200)
-  description = models.TextField()
+    done = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    date = models.DateField()
   
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'date']
--- todolist/views.py
+++ todolist/views.py
@@ -1,4 +1,5 @@
 import datetime
+from re import T
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
@@ -7,43 +8,53 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from todolist.models import Task
+from todolist.models import Task, TaskForm
 
 @login_required(login_url='/todolist/login')
 def home(request):
-    tasks = Task.objects.all()
-    context = {
-        'name': 'Daniel Christian Mandolang',
-        'npm': '2106630006',
-        'tasks': tasks
-    }
-    return render(request, 'todolist.html', context)
+    if (request.user.is_authenticated):
+        tasks = Task.objects.filter(user=request.user)
+        for task in tasks:
+            print(task.title, task.done)
+        context = {
+            'name': 'Daniel Christian Mandolang',
+            'npm': '2106630006',
+            'tasks': tasks
+        }
+        return render(request, 'todolist.html', context)
 
 @login_required(login_url='/todolist/login')
 def create_task(request):
+    form = TaskForm(request.POST)
+    context = {
+        'form': form
+    }
     return render(request, 'create_task.html')
 
 @login_required(login_url='/todolist/login')
 def create(request):
-    if (request.method == 'POST'):
-        # TODO: Get data from form
-        # TODO: Create new instance
-        form = request.POST.dict()
+    if (request.user.is_authenticated):
+        user = request.user
+        form = TaskForm(request.POST or None)
         if (form.is_valid()):
-            print("HERE")
-        return HttpResponseRedirect(reverse('todolist:'))
+            title = form.cleaned_data['title']
+            description = form.cleaned_data['description']
+            date = form.cleaned_data['date']
+            task = Task(title=title, description=description, date=date, user=user)
+            task.save()
+            return HttpResponseRedirect(reverse('todolist:home'))
 
 @login_required(login_url='/todolist/login')
 def mark_done(request, id):
-    task = Task.objects.filter(pk=id)
+    task = Task.objects.get(pk=id)
     task.done = not task.done
     task.save()
-    HttpResponseRedirect(reverse('todolist:'))
+    return HttpResponseRedirect(reverse('todolist:home'))
 
 @login_required(login_url='/todolist/login')
 def delete(request, id):
-    Task.objects.delete(pk=id)
-    HttpResponseRedirect(reverse('todolist:'))
+    Task.objects.filter(pk=id).delete()
+    return HttpResponseRedirect(reverse('todolist:home'))
 
 def login_user(request):
     if request.method == 'POST':
