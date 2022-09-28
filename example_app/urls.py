183a6126b348bc5fffa078ce3822403f6b8af354
danielcm585 <danielcm585@gmail.com>
2022-09-28 19:53:41 +0700

    Test deploy

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from example_app.views import index
+from example_app.views import *
 
 app_name = 'example_app'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('me/', show_me, name='show_me'),
 ]
--- example_app/views.py
+++ example_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def show_me(request):
+    return render(request, 'me.html')
